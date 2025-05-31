a2e97fa3a00289e52b5a90127721476d4d76acbc
jili-gulu-57 <2263293720@qq.com>
2025-05-31 20:37:53 +0800

    二叉树练习

---
+++ 二叉树练习（力扣145）/二叉树练习（力扣145）/源.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<vector>
+using namespace std;
+
+ struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    
+};
+
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> postorderTraversal(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+    void dfs(TreeNode* root)
+    {
+        if (root == nullptr)
+            return;
+
+        dfs(root->left);
+        dfs(root->right);
+        ans.push_back(root->val);
+    }
+};
