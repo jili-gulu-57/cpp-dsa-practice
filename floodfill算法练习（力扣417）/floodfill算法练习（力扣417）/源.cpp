11a4aa69411ee4bd05bff99222ae08dea6826029
jili-gulu-57 <2263293720@qq.com>
2025-12-01 00:10:43 +0800

    floodfill算法练习

--- floodfill算法练习（力扣417）/floodfill算法练习（力扣417）/源.cpp
+++ floodfill算法练习（力扣417）/floodfill算法练习（力扣417）/源.cpp
@@ -3,14 +3,16 @@
 using namespace std;
 
 
-//417.太平洋大西洋水流问题
+//417.太平洋大西洋水流问题（已通过）
 class Solution {
     int m, n;
+    int dx[4] = { 1,-1,0,0 };
+    int dy[4] = { 0,0,1,-1 };
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         m = heights.size(), n = heights[0].size();
-        vector<vector<bool>> p(m, vector<bool>(n));
-        vector<vector<bool>> a(m, vector<bool>(n));
+        vector<vector<bool>> p(m, vector<bool>(n,0));
+        vector<vector<bool>> a(m, vector<bool>(n,0));
         vector<vector<int>> ans;
 
         //统计第一个洋
@@ -24,11 +26,30 @@ public:
         for (int i = 0; i < n; i++)
             dfs(heights, m - 1, i, a);
 
-
+        //找出最终结果
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (p[i][j] && a[i][j])
+                    ans.push_back({ i,j });
+            }
+        }
+        return ans;
     }
 
-    void dfs(vector<vector<int>>& heights, int begin, int end, vector<vector<bool>>& oc)
+    void dfs(vector<vector<int>>& heights, int i, int j, vector<vector<bool>>& oc)
     {
-
+        oc[i][j] = true;
+        for (int k = 0; k < 4; k++)
+        {
+            int x = i + dx[k];
+            int y = j + dy[k];
+            if (x >= 0 && x < m && y >= 0 && y < n && heights[x][y] >= heights[i][j] && !oc[x][y])
+            {
+                oc[x][y] = true;
+                dfs(heights, x, y, oc);
+            }
+        }
     }
 };
