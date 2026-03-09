626745d7e28dd16f34798a42b9025c72b41a4907
jili-gulu-57 <2263293720@qq.com>
2026-03-09 23:52:06 +0800

    CSDN初稿（C++类和对象）

---
+++ CSDN初稿（C++类和对象）/CSDN初稿（C++类和对象）/源.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+struct Student
+{
+	char name[20];  //姓名
+	char num[20];   //学号
+	int age;	    //年龄
+	char sex;		//性别
+	//等等…………
+};
+
+int main()
+{
+	return 0;
+}
