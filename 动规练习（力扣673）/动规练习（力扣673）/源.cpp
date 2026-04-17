305b1a39e91b6b80440ef7359af07a01b7bb038e
jili-gulu-57 <2263293720@qq.com>
2026-04-17 20:44:24 +0800

    动规练习（力扣673）

---
+++ 动规练习（力扣673）/动规练习（力扣673）/源.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//673.最长递增子序列的个数（已通过）
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> len(n, 1), count(n, 1);
+        int retlen = 1, retcount = 1;
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[j] < nums[i])
+                {
+                    if (len[j] + 1 == len[i])
+                    {
+                        count[i] += count[j];
+                    }
+                    else if (len[j] + 1 > len[i])
+                    {
+                        len[i] = len[j] + 1;
+                        count[i] = count[j];
+                    }
+                }
+            }
+            if (retlen == len[i])
+                retcount += count[i];
+            else if (retlen < len[i])
+            {
+                retlen = len[i];
+                retcount = count[i];
+            }
+        }
+        return retcount;
+    }
+};
